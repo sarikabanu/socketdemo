2a0b9fbc94bcf3e81608121897309ff3d4227a82
sarikabanu <sarika.b@shrigowri.com>
2018-03-01 15:20:10 +0530

    index

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.get('/testsocket',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile(./index.html);
 })
 io.on('connection',function(socket){
     console.log('one user connected '+socket.id);
